2e7b282526a4bffd5e7f5bdc4e6e216063c8898d
Olivier Giniaux <oginiaux@gmail.com>
2019-05-16 13:41:39 +0200

    Migrate everything in a single .exe for convenience

--- Source/UnityPack/Program.cs
+++ Source/UnityPack/Program.cs
@@ -1,34 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace UnityPacker
-{
-    class PackProgram
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
+namespace UnityPacker {
+
+    class PackProgram {
+
+        static void Main(string[] args) {
+
+            if (args.Length < 2) {
                 Console.WriteLine("Usage:");
-                Console.WriteLine(
-                    "UnityPacker [Source Path] [Package Name] [Respect Meta] [Omitted Extensions] [Omitted Directories]");
+                Console.WriteLine("UnityPacker mode=pack folder=\"\" name=\"Package\" root=\"\" exts=\"\" dirs=\"\"");
                 return;
             }
 
-            var inpath = args[0];
-            var fileName = args.Length > 1 ? args[1] : "Package";
-            var rootDir = args.Length > 2 ? args[2] : "Assets/";
-            var exts = args.Length > 3 ? args[3].Split(',') : new string[0];
-            var dirs = args.Length > 4 ? args[4].Split(',') : new string[0];
+            var parsedArgs = new Dictionary<string, string>();
+            foreach (string arg in args) {
+                var split = arg.Split(new[] { '=' });
+                if (split.Length == 2) {
+                    parsedArgs.Add(split[0], split[1].Trim(new[] { '"', '\'' }));
+                }
+            }
 
-            var extensions = new List<string>(exts)
-            {
-                "meta" // always skip meta files
-            };
+            string getArg(string key) {
+                if (parsedArgs.ContainsKey(key)) {
+                    return parsedArgs[key];
+                }
+                throw new Exception($"Argument '{key}' is missing !");
+            }
+            
+            switch (getArg("mode").ToLower()) {
+                case "p":
+                case "pck":
+                case "pack":
+                    Pack(getArg("folder"), getArg("name"), getArg("root"), getArg("ignore"));
+                    break;
+                case "u":
+                case "upck":
+                case "unpack":
+                    Unpack(getArg("package"), getArg("destination"));
+                    break;
+                default:
+                    Console.WriteLine("mode should be either 'pack' or 'unpack'");
+                    break;
+            }
+        }
 
-            // Create a package object from the given directory
-            var pack = Package.FromDirectory(inpath, fileName, true, extensions.ToArray(), dirs);
+        static void Pack(string folderToPack, string packageName, string rootDir, string ignoreRegex) {
+            var pack = Package.FromDirectory(folderToPack, packageName, true, ignoreRegex);
             pack.GeneratePackage(rootDir);
+        }
+
+        static void Unpack(string packagePath, string destinationFolder) {
+            var p = Package.FromPackage(packagePath);
+            p.GenerateFolder(destinationFolder ?? "");
         }
     }
 }
